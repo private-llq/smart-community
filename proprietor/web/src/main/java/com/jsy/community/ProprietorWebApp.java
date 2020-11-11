77a21de2610b8171847d7e44a24cf886fed69627
李欢 <503580622@qq.com>
2020-11-11 16:03:19 +0800

    切换文档

--- proprietor/web/src/main/java/com/jsy/community/ProprietorWebApp.java
+++ proprietor/web/src/main/java/com/jsy/community/ProprietorWebApp.java
@@ -4,11 +4,9 @@ package com.jsy.community;
 import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import springfox.documentation.oas.annotations.EnableOpenApi;
 
 @SpringBootApplication
 @DubboComponentScan
-@EnableOpenApi
 public class ProprietorWebApp {
 	public static void main(String[] args) {
 		SpringApplication.run(ProprietorWebApp.class, args);
--- proprietor/web/src/main/java/com/jsy/community/config/Swagger3.java
+++ proprietor/web/src/main/java/com/jsy/community/config/Swagger3.java
@@ -1,17 +1,21 @@
 package com.jsy.community.config;
 
+import com.github.xiaoymin.knife4j.spring.annotations.EnableKnife4j;
 import io.swagger.annotations.ApiOperation;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.oas.annotations.EnableOpenApi;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 
 @Configuration
+@EnableOpenApi
+@EnableKnife4j
 public class Swagger3 {
 	@Bean
 	public Docket createRestApi() {
