e703bb76994426d465b0259e6a697bed419add03
Yu Linfeng <yu1650056068@gmail.com>
2020-11-12 15:31:11 +0800

    首次增加车辆图片上传接口

--- proprietor/web/src/main/java/com/jsy/community/controller/CarController.java
+++ proprietor/web/src/main/java/com/jsy/community/controller/CarController.java
@@ -11,9 +11,15 @@ import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.io.FilenameUtils;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -28,8 +34,15 @@ public class CarController {
     @DubboReference(version = Const.version, group = Const.group, check = false)
     private ICarService carService;
 
+    //允许上传的文件后缀种类，临时，后续从配置文件中取值
+    private final String[] carImageAllowSuffix = new String[]{"jpg", "jpeg", "png"};
+
+    //允许上传的文件大小，临时，后续从配置文件中取值 由Spring控制
+    private final int carImageMaxSizeKB = 500;
+
     /**
      * 新增业主固定车辆
+     *
      * @param carEntity 前端参数对象
      * @return 返回新增结果
      */
@@ -49,6 +62,7 @@ public class CarController {
 
     /**
      * 修改业主固定车辆
+     *
      * @param carEntity 前端请求参数对象
      * @return 返回修改影响行数
      */
@@ -64,6 +78,7 @@ public class CarController {
     /**
      * 业主车辆分页查询
      * 所有参数类型为String 不由Spring处理 自定义处理效验
+     *
      * @param page        当前页
      * @param pageSize    页显示条数
      * @param uid         业主id
@@ -74,7 +89,7 @@ public class CarController {
     @GetMapping(value = "/car/{uid}/{checkStatus}/{page}/{pageSize}")
     public CommonResult<?> queryProprietorCar(@PathVariable("uid") String uid, @PathVariable("checkStatus") String checkStatus, @PathVariable("page") String page, @PathVariable("pageSize") String pageSize) {
         //参数数字效验
-        if(isNumber(uid) && isNumber(page) && isNumber(pageSize) && isNumber(checkStatus)){
+        if (isNumber(uid) && isNumber(page) && isNumber(pageSize) && isNumber(checkStatus)) {
             //Map传递请求参数
             Map<String, Object> paramMap = new HashMap<>(4);
             paramMap.put("page", page);
@@ -91,15 +106,17 @@ public class CarController {
 
     /**
      * 判断字符串是否是一个完整的数字
-     * @param str   字符串
-     * @return      返回这个字符串是否是字符串的布尔值
+     *
+     * @param str 字符串
+     * @return 返回这个字符串是否是字符串的布尔值
      */
-    private Boolean isNumber(String str){
+    private Boolean isNumber(String str) {
         return Pattern.compile("^-?\\d+(\\.\\d+)?$").matcher(str).matches();
     }
 
     /**
      * 根据车牌号检查车辆是否已经登记
+     *
      * @param carPlate 车牌号
      * @return 返回是否存在布尔值
      */
@@ -109,6 +126,7 @@ public class CarController {
 
     /**
      * 通过车辆ID 删除 车辆方法
+     *
      * @param id 车辆id
      * @return 返回逻辑删除影响行
      */
@@ -119,6 +137,44 @@ public class CarController {
         return res > 0 ? CommonResult.ok(res) : CommonResult.error(JSYError.DUPLICATE_KEY.getCode(), "车辆不存在!");
     }
 
-
+    @ApiOperation("所属人车辆图片上传接口")
+    @PostMapping("/car/carImageUpload")
+    public CommonResult<?> carImageUpload(MultipartFile carImage) {
+        //1.接口非空验证
+        if (null == carImage) {
+            return CommonResult.error(JSYError.BAD_REQUEST);
+        }
+        //2.文件大小验证
+        long fileSizeForKB = carImage.getSize() / 1024;
+        if (fileSizeForKB > carImageMaxSizeKB) {
+            return CommonResult.error(JSYError.REQUEST_PARAM.getCode(), "文件太大了,最大：" + carImageMaxSizeKB + "KB");
+        }
+        String fileName = carImage.getOriginalFilename();
+        log.info("车辆图片上传文件名：" + fileName + " 车辆图片文件大小：" + fileSizeForKB + "KB");
+        //3.文件后缀验证
+        boolean extension = FilenameUtils.isExtension(fileName, carImageAllowSuffix);
+        if (!extension) {
+            return CommonResult.error(JSYError.REQUEST_PARAM.getCode(), "文件后缀不允许,可用后缀" + Arrays.asList(carImageAllowSuffix));
+        }
+        //4.临时本地上传方式
+        FileOutputStream fileOutputStream = null;
+        try {
+            byte[] bytes = carImage.getBytes();
+            fileOutputStream = new FileOutputStream(new File("D:" + File.separator + "TestFileDirectory" + File.separator + fileName));
+            fileOutputStream.write(bytes);
+            //上传成功返回访问路径
+            return CommonResult.ok("https://www.baidu.com/" + fileName);
+        } catch (IOException e) {
+            return CommonResult.error(JSYError.NOT_IMPLEMENTED.getCode(), "文件上传失败!" + e.getMessage());
+        } finally {
+            if (fileOutputStream != null) {
+                try {
+                    fileOutputStream.close();
+                } catch (IOException e1) {
+                    e1.printStackTrace();
+                }
+            }
+        }
+    }
 
 }
