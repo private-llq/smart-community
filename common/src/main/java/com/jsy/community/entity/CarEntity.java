81b29a82dbcaf24db3b376786e53df1ba5e544c1
Yu Linfeng <yu1650056068@gmail.com>
2020-11-12 09:40:08 +0800

    车辆实体 更新效验更新车辆验证

--- common/src/main/java/com/jsy/community/entity/CarEntity.java
+++ common/src/main/java/com/jsy/community/entity/CarEntity.java
@@ -27,30 +27,30 @@ public class CarEntity extends BaseEntity {
     @NotNull(groups = {addCarValidated.class}, message = "用户id必须不为空!")
     private Long uid;
 
-    @NotNull(groups = {addCarValidated.class}, message = "车位id必须不为空!")
+    @NotNull(groups = {addCarValidated.class, updateCarValidated.class}, message = "车位id必须不为空!")
     @ApiModelProperty(value = "车位ID")
     private Long carPositionId;
 
-    @NotNull(groups = {addCarValidated.class}, message = "社区id必须不为空!")
+    @NotNull(groups = {addCarValidated.class, updateCarValidated.class}, message = "社区id必须不为空!")
     @ApiModelProperty(value = "社区ID")
     private Long communityId;
 
-    @Pattern(groups = {addCarValidated.class}, regexp = "^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[A-Z0-9]{4}[A-Z0-9挂学警港澳]{1}$", message = "请输入一个正确的车牌号!")
+    @Pattern(groups = {addCarValidated.class, updateCarValidated.class}, regexp = "^[京津沪渝冀豫云辽黑湘皖鲁新苏浙赣鄂桂甘晋蒙陕吉闽贵粤青藏川宁琼使领A-Z]{1}[A-Z]{1}[A-Z0-9]{4}[A-Z0-9挂学警港澳]{1}$", message = "请输入一个正确的车牌号!")
     @ApiModelProperty(value = "车辆牌照")
     private String carPlate;
 
     @ApiModelProperty(value = "车辆照片访问路径")
     private String carImageUrl;
 
-    @Pattern(groups = {addCarValidated.class}, regexp = "^1[3|4|5|7|8][0-9]{9}$", message = "请输入一个正确的手机号码 电信丨联通丨移动!")
+    @Pattern(groups = {addCarValidated.class, updateCarValidated.class}, regexp = "^1[3|4|5|7|8][0-9]{9}$", message = "请输入一个正确的手机号码 电信丨联通丨移动!")
     @ApiModelProperty(value = "车主联系方式")
     private String contact;
 
-    @NotBlank(groups = {addCarValidated.class}, message = "车辆所属人不能为空!")
+    @NotBlank(groups = {addCarValidated.class, updateCarValidated.class}, message = "车辆所属人不能为空!")
     @ApiModelProperty(value = "车辆所属人")
     private String owner;
 
-    @NotBlank(groups = {addCarValidated.class}, message = "车辆类型未选择!")
+    @NotBlank(groups = {addCarValidated.class, updateCarValidated.class}, message = "车辆类型未选择!")
     @ApiModelProperty(value = "车辆类型")
     private String carType;
 
@@ -66,5 +66,10 @@ public class CarEntity extends BaseEntity {
      * 登记车辆前端参数验证接口
      */
     public interface addCarValidated{}
+
+    /**
+     * 更新车辆前端参数验证接口
+     */
+    public interface updateCarValidated{}
 
 }
