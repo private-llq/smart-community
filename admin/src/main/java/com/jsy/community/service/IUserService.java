b763bf08934596328da61bf094350ab30cc4ce58
李欢 <503580622@qq.com>
2020-11-19 16:58:42 +0800

    后台业主

---
+++ admin/src/main/java/com/jsy/community/service/IUserService.java
@@ -0,0 +1,13 @@
+package com.jsy.community.service;
+
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.jsy.community.entity.UserEntity;
+
+/**
+ * 业主接口
+ *
+ * @author ling
+ * @since 2020-11-19 16:49
+ */
+public interface IUserService extends IService<UserEntity> {
+}
