d288be5dc88e2f323fc42d44c5048984ddf4937b
李欢 <503580622@qq.com>
2020-11-19 11:00:24 +0800

    异常处理

--- common/src/main/java/com/jsy/community/exception/JSYError.java
+++ common/src/main/java/com/jsy/community/exception/JSYError.java
@@ -13,6 +13,7 @@ public enum JSYError {
 	UNAUTHORIZED(ConstError.UNAUTHORIZED, "未认证"),
 	FORBIDDEN(ConstError.FORBIDDEN, "禁止访问"),
 	NOT_FOUND(ConstError.NOT_FOUND, "页面丢失了"),
+	NOT_SUPPORT_REQUEST_METHOD(ConstError.NOT_SUPPORT_REQUEST_METHOD, "不支持的请求类型"),
 	REQUEST_PARAM(ConstError.REQUEST_PARAM, "请求参数错误"),
 	INTERNAL(ConstError.INTERNAL, "服务器错误"),
 	NOT_IMPLEMENTED(ConstError.NOT_IMPLEMENTED, "未实现"),
