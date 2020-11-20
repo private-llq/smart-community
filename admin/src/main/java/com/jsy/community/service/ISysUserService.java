dbe6550683109217424c9763fc445aa5d94d12b1
李欢 <503580622@qq.com>
2020-11-20 18:01:10 +0800

    后台登录，用户控制器，文件上传，oss配置

---
+++ admin/src/main/java/com/jsy/community/service/ISysUserService.java
@@ -0,0 +1,59 @@
+package com.jsy.community.service;
+
+import com.baomidou.mybatisplus.core.metadata.IPage;
+import com.baomidou.mybatisplus.extension.service.IService;
+import com.jsy.community.entity.SysUserEntity;
+import com.jsy.community.qo.BaseQO;
+import com.jsy.community.qo.admin.NameAndCreatorQO;
+
+import java.util.List;
+
+
+/**
+ * 系统用户
+ */
+public interface ISysUserService extends IService<SysUserEntity> {
+	
+	IPage<SysUserEntity> queryPage(BaseQO<NameAndCreatorQO> qo);
+	
+	/**
+	 * 查询用户的所有权限
+	 *
+	 * @param userId 用户ID
+	 */
+	List<String> queryAllPerms(Long userId);
+	
+	/**
+	 * 查询用户的所有菜单ID
+	 */
+	List<Long> queryAllMenuId(Long userId);
+	
+	/**
+	 * 根据用户名，查询系统用户
+	 */
+	SysUserEntity queryByUserName(String username);
+	
+	/**
+	 * 保存用户
+	 */
+	void saveUser(SysUserEntity user);
+	
+	/**
+	 * 修改用户
+	 */
+	void update(SysUserEntity user);
+	
+	/**
+	 * 删除用户
+	 */
+	void deleteBatch(Long[] userIds);
+	
+	/**
+	 * 修改密码
+	 *
+	 * @param userId      用户ID
+	 * @param password    原密码
+	 * @param newPassword 新密码
+	 */
+	boolean updatePassword(Long userId, String password, String newPassword);
+}
---
+++ common/src/main/java/com/jsy/community/entity/SysUserTokenEntity.java
@@ -0,0 +1,28 @@
+package com.jsy.community.entity;
+
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+
+import java.time.LocalDateTime;
+
+
+/**
+ * 系统用户Token
+ */
+@EqualsAndHashCode(callSuper = true)
+@Data
+@TableName("t_sys_user_token")
+public class SysUserTokenEntity extends BaseEntity {
+	
+	//用户ID
+	@TableId(type = IdType.INPUT)
+	private Long userId;
+	//token
+	private String token;
+	//过期时间
+	private LocalDateTime expireTime;
+	
+}
