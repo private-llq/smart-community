a8f1aef84230f52ffe7da472d81bfffe3ce88f0c
李浩 <javalihao_233@163.com>
2020-11-25 16:43:41 +0800

    修改userHouse

--- property/property-api/src/main/java/com/jsy/community/api/IUserHouseService.java
+++ property/property-api/src/main/java/com/jsy/community/api/IUserHouseService.java
@@ -1,7 +1,9 @@
 package com.jsy.community.api;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.jsy.community.entity.UserHouseEntity;
+import com.jsy.community.qo.BaseQO;
 
 /**
  * <p>
@@ -12,5 +14,7 @@ import com.jsy.community.entity.UserHouseEntity;
  * @since 2020-11-25
  */
 public interface IUserHouseService extends IService<UserHouseEntity> {
-
+	
+	Page<UserHouseEntity> selectUserHouse(BaseQO<UserHouseEntity> baseQO,Long communityId);
+	
 }
