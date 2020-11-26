855ecce1e51c2bb96404c43d5b88d125fc06427d
qq459799974 <459799974@qq.com>
2020-11-26 14:09:07 +0800

    前台传参异常捕获

--- proprietor/proprietor-web/src/main/java/com/jsy/community/exception/ProprietorExceptionHandler.java
+++ proprietor/proprietor-web/src/main/java/com/jsy/community/exception/ProprietorExceptionHandler.java
@@ -3,6 +3,7 @@ package com.jsy.community.exception;
 import com.jsy.community.api.ProprietorException;
 import com.jsy.community.vo.CommonResult;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -27,5 +28,11 @@ public class ProprietorExceptionHandler extends JSYExceptionHandler {
 			return CommonResult.error(((ProprietorException) e).getCode(), e.getMessage());
 		}
 		return CommonResult.error(JSYError.INTERNAL);
+	}
+	
+	@ExceptionHandler(HttpMessageNotReadableException.class)
+	public CommonResult<Boolean> handleException(HttpMessageNotReadableException e) {
+		log.error(e.getMessage(), e);
+		return CommonResult.error(JSYError.REQUEST_PARAM);
 	}
 }
