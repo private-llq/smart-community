817810d697b6551c01aa2630e9c5bf25ee687803
qq459799974 <459799974@qq.com>
2020-11-27 13:57:27 +0800

    业主端数据重复异常捕获

--- proprietor/proprietor-web/src/main/java/com/jsy/community/exception/ProprietorExceptionHandler.java
+++ proprietor/proprietor-web/src/main/java/com/jsy/community/exception/ProprietorExceptionHandler.java
@@ -3,6 +3,7 @@ package com.jsy.community.exception;
 import com.jsy.community.api.ProprietorException;
 import com.jsy.community.vo.CommonResult;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.dao.DuplicateKeyException;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
@@ -34,5 +35,11 @@ public class ProprietorExceptionHandler extends JSYExceptionHandler {
 	public CommonResult<Boolean> handleException(HttpMessageNotReadableException e) {
 		log.error(e.getMessage(), e);
 		return CommonResult.error(JSYError.REQUEST_PARAM);
+	}
+	
+	@ExceptionHandler(DuplicateKeyException.class)
+	public CommonResult<Boolean> handleDuplicateKeyException(DuplicateKeyException e) {
+		log.error(e.getMessage(), e);
+		return CommonResult.error(JSYError.DUPLICATE_KEY);
 	}
 }
