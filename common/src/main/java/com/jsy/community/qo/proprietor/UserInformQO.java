547e614f5f0ff74349967a3b8e6f9734f447d16e
hu-yunfeng <1056134646@qq.com>
2020-12-07 15:55:13 +0800

    添加已读未读消息查询接口-二次提交

--- common/src/main/java/com/jsy/community/qo/proprietor/UserInformQO.java
+++ common/src/main/java/com/jsy/community/qo/proprietor/UserInformQO.java
@@ -2,6 +2,7 @@ package com.jsy.community.qo.proprietor;
 
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
 
 import java.io.Serializable;
 
@@ -12,13 +13,14 @@ import java.io.Serializable;
  * @create: 2020-12-07 09:50
  **/
 @ApiModel("消息已读状态查询")
+@Data
 public class UserInformQO implements Serializable {
 
-    @ApiModelProperty("查询页数")
-    private Integer pageNo=2;
+    @ApiModelProperty("分页查询当前页----暂时没用")
+    private Long page=0l;
 
-    @ApiModelProperty("查询每页条数")
-    private Integer pageSize=10;
+    @ApiModelProperty("分页查询每页数据条数----暂时没用")
+    private Long size=10l;
 
     @ApiModelProperty("按通知ID查询")
     private Long informId;
@@ -35,59 +37,4 @@ public class UserInformQO implements Serializable {
     @ApiModelProperty("按房间ID查询")
     private Long houseId;
 
-    public Integer getPageNo() {
-        return pageNo;
-    }
-
-    public void setPageNo(Integer pageNo) {
-        this.pageNo = pageNo;
-    }
-
-    public Integer getPageSize() {
-        return pageSize;
-    }
-
-    public void setPageSize(Integer pageSize) {
-        this.pageSize = pageSize;
-    }
-
-    public Long getInformId() {
-        return informId;
-    }
-
-    public void setInformId(Long informId) {
-        this.informId = informId;
-    }
-
-    public String getUserName() {
-        return userName;
-    }
-
-    public void setUserName(String userName) {
-        this.userName = userName;
-    }
-
-    public Integer getInformStatus() {
-        return informStatus;
-    }
-
-    public void setInformStatus(Integer informStatus) {
-        this.informStatus = informStatus;
-    }
-
-    public Long getCommunityId() {
-        return communityId;
-    }
-
-    public void setCommunityId(Long communityId) {
-        this.communityId = communityId;
-    }
-
-    public Long getHouseId() {
-        return houseId;
-    }
-
-    public void setHouseId(Long houseId) {
-        this.houseId = houseId;
-    }
 }
