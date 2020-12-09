1d914fa17c7d1a4007026405424ecec370293a2d
qq459799974 <459799974@qq.com>
2020-12-09 11:06:00 +0800

    删除不必要的代码

--- proprietor/proprietor-web/src/main/java/com/jsy/community/controller/HouseMemberController.java
+++ proprietor/proprietor-web/src/main/java/com/jsy/community/controller/HouseMemberController.java
@@ -22,21 +22,21 @@ import java.util.List;
 
 /**
  * <p>
- * 房间成员表 前端控制器
+ * 房间成员表 前端控制器 (此套房主邀请加入房间(添加房间成员)的方案暂时搁置)
  * </p>
  *
  * @author qq459799974
  * @since 2020-11-23
  */
-@Api(tags = "房间成员控制器")
-@RestController
-@RequestMapping("houseMember")
-@ApiJSYController
-@Login
+//@Api(tags = "房间成员控制器")
+//@RestController
+//@RequestMapping("houseMember")
+//@ApiJSYController
+//@Login
 public class HouseMemberController {
 	
-	@DubboReference(version = Const.version, group = Const.group, check = false)
-	private IHouseMemberService iHouseMemberService;
+//	@DubboReference(version = Const.version, group = Const.group, check = false)
+//	private IHouseMemberService iHouseMemberService;
 	
 //	@ApiOperation("【房间成员】邀请")
 //	@PostMapping("")
@@ -53,15 +53,15 @@ public class HouseMemberController {
 //		return result ? CommonResult.ok() : CommonResult.error(JSYError.INTERNAL.getCode(),"房间成员邀请失败");
 //	}
 	
-	@ApiOperation("【房间成员】批量删除成员/撤销邀请")
-	@DeleteMapping("")
-	public CommonResult deleteHouseMember(@RequestBody List<Long> ids){
-		if(CollectionUtils.isEmpty(ids)){
-			return CommonResult.error(JSYError.REQUEST_PARAM);
-		}
-		boolean result = iHouseMemberService.deleteHouseMember(ids);
-		return result ? CommonResult.ok() : CommonResult.error(JSYError.INTERNAL.getCode(),"操作失败");
-	}
+//	@ApiOperation("【房间成员】批量删除成员/撤销邀请")
+//	@DeleteMapping("")
+//	public CommonResult deleteHouseMember(@RequestBody List<Long> ids){
+//		if(CollectionUtils.isEmpty(ids)){
+//			return CommonResult.error(JSYError.REQUEST_PARAM);
+//		}
+//		boolean result = iHouseMemberService.deleteHouseMember(ids);
+//		return result ? CommonResult.ok() : CommonResult.error(JSYError.INTERNAL.getCode(),"操作失败");
+//	}
 	
 //	@ApiOperation("【房间成员】接受邀请")
 //	@PutMapping("")
@@ -73,12 +73,12 @@ public class HouseMemberController {
 //		return result ? CommonResult.ok() : CommonResult.error(JSYError.INTERNAL.getCode(),"加入失败");
 //	}
 	
-	@ApiOperation("【房间成员】查询成员/查询邀请")
-	@PostMapping("page")
-	public CommonResult<Page<HouseMemberEntity>> queryHouseMemberPage(@RequestBody BaseQO<HouseMemberQO> baseQO){
-		baseQO.getQuery().setUid(JwtUtils.getUserId());
-		return CommonResult.ok(iHouseMemberService.queryHouseMemberPage(baseQO));
-	}
+//	@ApiOperation("【房间成员】查询成员/查询邀请")
+//	@PostMapping("page")
+//	public CommonResult<Page<HouseMemberEntity>> queryHouseMemberPage(@RequestBody BaseQO<HouseMemberQO> baseQO){
+//		baseQO.getQuery().setUid(JwtUtils.getUserId());
+//		return CommonResult.ok(iHouseMemberService.queryHouseMemberPage(baseQO));
+//	}
 	
 }
 
