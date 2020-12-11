c9d446e7af0a4bfcfd18521f78302219b9daa712
李浩 <javalihao_233@163.com>
2020-12-11 17:44:50 +0800

    完成app端缴费公司单位的接口

---
+++ proprietor/proprietor-service/src/main/java/com/jsy/community/service/impl/PayTypeServiceImpl.java
@@ -0,0 +1,95 @@
+package com.jsy.community.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
+import com.jsy.community.api.IPayTypeService;
+import com.jsy.community.constant.Const;
+import com.jsy.community.entity.PayCompanyEntity;
+import com.jsy.community.entity.PayTypeEntity;
+import com.jsy.community.exception.JSYException;
+import com.jsy.community.mapper.PayCompanyMapper;
+import com.jsy.community.mapper.PayTypeMapper;
+import com.jsy.community.mapper.RegionMapper;
+import com.jsy.community.qo.BaseQO;
+import com.jsy.community.utils.PageInfo;
+import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.CollectionUtils;
+import org.springframework.util.StringUtils;
+
+import javax.annotation.Resource;
+import java.util.List;
+
+/**
+ * <p>
+ * 缴费类型 服务实现类
+ * </p>
+ *
+ * @author lihao
+ * @since 2020-12-11
+ */
+@DubboService(version = Const.version, group = Const.group_proprietor)
+public class PayTypeServiceImpl extends ServiceImpl<PayTypeMapper, PayTypeEntity> implements IPayTypeService {
+	@Resource
+	private PayTypeMapper payTypeMapper;
+	
+	@Resource
+	private PayCompanyMapper companyMapper;
+	
+	@Resource
+	private RegionMapper regionMapper;
+	
+	@Override
+	public List<PayTypeEntity> getPayTypes(Long id) {
+		// 根据城市id 获取其下 缴费类型id集合
+		List<Long> list = regionMapper.getListPayTypeId(id);
+		
+		if (CollectionUtils.isEmpty(list)) {
+			return null;
+		}
+		// 根据id批量查询缴费类型
+		return payTypeMapper.selectBatchIds(list);
+	}
+	
+	@Override
+	@Transactional
+	public void addPayType(Long id, PayTypeEntity payType) {
+		String name = payType.getName();
+		QueryWrapper<PayTypeEntity> wrapper = new QueryWrapper<>();
+		wrapper.eq("name", name);
+		PayTypeEntity payTypeEntity = payTypeMapper.selectOne(wrapper);
+		if (payTypeEntity != null) {
+			Long dataId = payTypeEntity.getId(); // 缴费类型id
+			
+			// 拿到这两个玩意去中间表查 如果有说明已经添加过了
+			Integer count = payTypeMapper.selectMiddle(id, dataId);
+			if (count > 0) {
+				throw new JSYException("对不起,该城市的缴费类型已经添加过了");
+			}
+			payTypeMapper.insertToMiddle(id, payTypeEntity.getId()); // 设置关联关系
+		} else {
+			PayTypeEntity entity = new PayTypeEntity();
+			BeanUtils.copyProperties(payType, entity);
+			payTypeMapper.insert(entity); // 添加一个新缴费类型
+			payTypeMapper.insertToMiddle(id, entity.getId()); // 设置关联关系
+		}
+	}
+	
+	@Override
+	public PageInfo<PayCompanyEntity> getPayCompany(BaseQO<PayCompanyEntity> baseQO, Long type, Long cityId) {
+		String cityName = baseQO.getQuery().getName();
+		Page<PayCompanyEntity> page = new Page<>(baseQO.getPage(), baseQO.getSize());
+		QueryWrapper<PayCompanyEntity> wrapper = new QueryWrapper<>();
+		if (!StringUtils.isEmpty(cityName)) {
+			wrapper.eq("type_id", type).eq("region_id", cityId).like("name", cityName);
+		} else {
+			wrapper.eq("type_id", type).eq("region_id", cityId);
+		}
+		companyMapper.selectPage(page, wrapper);
+		PageInfo<PayCompanyEntity> info = new PageInfo<>();
+		BeanUtils.copyProperties(page,info);
+		return info;
+	}
+}
---
+++ proprietor/proprietor-web/src/main/java/com/jsy/community/controller/PayCompanyController.java
@@ -0,0 +1,58 @@
+//package com.jsy.community.controller;
+//
+//
+//import com.jsy.community.annotation.ApiJSYController;
+//import com.jsy.community.api.IPayCompanyService;
+//import com.jsy.community.constant.Const;
+//import com.jsy.community.entity.PayCompanyEntity;
+//import com.jsy.community.qo.BaseQO;
+//import com.jsy.community.utils.PageInfo;
+//import com.jsy.community.vo.CommonResult;
+//import io.swagger.annotations.Api;
+//import io.swagger.annotations.ApiOperation;
+//import lombok.extern.slf4j.Slf4j;
+//import org.apache.dubbo.config.annotation.DubboReference;
+//import org.springframework.web.bind.annotation.PostMapping;
+//import org.springframework.web.bind.annotation.RequestBody;
+//import org.springframework.web.bind.annotation.RequestMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+///**
+// * <p>
+// * 缴费单位 前端控制器
+// * </p>
+// *
+// * @author lihao
+// * @since 2020-12-11
+// */
+//@Api(tags = "缴费单位控制器")
+//@Slf4j
+//@RestController
+//@RequestMapping("/payCompany")
+//@ApiJSYController
+//public class PayCompanyController {
+//
+//	@DubboReference(version = Const.version, group = Const.group_proprietor, check = false)
+//	private IPayCompanyService payCompanyService;
+//
+//	@ApiOperation("添加缴费单位")
+//	@PostMapping("/addPayCompany")
+//	public CommonResult addPayCompany(@RequestBody PayCompanyEntity companyEntity){
+//		payCompanyService.addPayCompany(companyEntity);
+//		return CommonResult.ok();
+//	}
+//
+//	@ApiOperation("查询所有缴费单位信息")
+//	@PostMapping("/getPayCompany")  // TODO 做了一个根据缴费单位名称的模糊分页查询
+//	public CommonResult<PageInfo<PayCompanyEntity>> getPayCompany(@RequestBody BaseQO<PayCompanyEntity> baseQO){
+//		PageInfo<PayCompanyEntity> pageInfo = payCompanyService.getPayCompany(baseQO);
+//		return CommonResult.ok(pageInfo);
+//	}
+//
+//
+//
+//
+//
+//
+//}
+//
