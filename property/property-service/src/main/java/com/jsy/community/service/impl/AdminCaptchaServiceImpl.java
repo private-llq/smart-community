b39a78afce0ff31d3d2f0c3c5795ef8a1fc101de
qq459799974 <459799974@qq.com>
2020-12-18 10:54:47 +0800

    物业端角色菜单用户修改

--- property/property-service/src/main/java/com/jsy/community/service/impl/AdminCaptchaServiceImpl.java
+++ property/property-service/src/main/java/com/jsy/community/service/impl/AdminCaptchaServiceImpl.java
@@ -5,7 +5,7 @@ import cn.hutool.core.date.DateUtil;
 import cn.hutool.core.util.StrUtil;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
-import com.google.code.kaptcha.Producer;
+//import com.google.code.kaptcha.Producer;
 import com.jsy.community.api.IAdminCaptchaService;
 import com.jsy.community.constant.Const;
 import com.jsy.community.entity.admin.AdminCaptchaEntity;
@@ -22,30 +22,30 @@ import java.util.Date;
  */
 @DubboService(version = Const.version, group = Const.group_property)
 public class AdminCaptchaServiceImpl extends ServiceImpl<AdminCaptchaMapper, AdminCaptchaEntity> implements IAdminCaptchaService {
-	@Resource
-	private Producer producer;
+//	@Resource
+//	private Producer producer;
 	
 	@Resource
 	private AdminCaptchaMapper adminCaptchaMapper;
 	
-	@Override
-	public BufferedImage getCaptcha(String uuid) {
-		if (StrUtil.isBlank(uuid)) {
-			throw new JSYException("uuid不能为空");
-		}
-		// 生成文字验证码
-		String code = producer.createText();
-		
-		AdminCaptchaEntity captchaEntity = new AdminCaptchaEntity();
-		captchaEntity.setUuid(uuid);
-		captchaEntity.setCode(code);
-		// 5分钟后过期
-		captchaEntity.setExpireTime(DateUtil.offset(new Date(), DateField.HOUR, 5));
-		adminCaptchaMapper.updateCaptcha(captchaEntity);
+//	@Override
+//	public BufferedImage getCaptcha(String uuid) {
+//		if (StrUtil.isBlank(uuid)) {
+//			throw new JSYException("uuid不能为空");
+//		}
+//		// 生成文字验证码
+//		String code = producer.createText();
+//
+//		AdminCaptchaEntity captchaEntity = new AdminCaptchaEntity();
+//		captchaEntity.setUuid(uuid);
+//		captchaEntity.setCode(code);
+//		// 5分钟后过期
+//		captchaEntity.setExpireTime(DateUtil.offset(new Date(), DateField.HOUR, 5));
+//		adminCaptchaMapper.updateCaptcha(captchaEntity);
 //		this.save(captchaEntity);
 		
-		return producer.createImage(code);
-	}
+//		return producer.createImage(code);
+//	}
 	
 	@Override
 	public boolean validate(String uuid, String code) {
