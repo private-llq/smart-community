54f581f962d6426e2a529f3c4ad217d9060ff0d9
Yu Linfeng <yu1650056068@gmail.com>
2020-12-19 16:47:18 +0800

    增加业务层异常处理类

---
+++ lease/lease-api/src/main/java/com/jsy/lease/api/LeaseException.java
@@ -0,0 +1,27 @@
+package com.jsy.lease.api;
+
+import com.jsy.community.constant.ConstError;
+import com.jsy.community.exception.JSYError;
+import com.jsy.community.exception.JSYException;
+
+/**
+ * @author ling
+ * @since 2020-11-11 14:13
+ */
+public class LeaseException extends JSYException {
+	public LeaseException(Integer code, String message) {
+		super(code, message);
+	}
+
+	public LeaseException() {
+		super();
+	}
+
+	public LeaseException(String message) {
+		super(ConstError.NORMAL, message);
+	}
+
+	public LeaseException(JSYError error) {
+		super(error);
+	}
+}
