6c1e0888fc31a40c306c1b6d88e651ee4e6c60db
Yu Linfeng <yu1650056068@gmail.com>
2020-12-19 11:17:39 +0800

    新增房屋VO返回对象

---
+++ common/src/main/java/com/jsy/community/vo/HouseVo.java
@@ -0,0 +1,45 @@
+package com.jsy.community.vo;
+
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * 房屋列表信息
+ * @author YuLF
+ * @since 2020-12-18 15:38
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value="House返回对象", description="社区房屋返回对象")
+public class HouseVo implements Serializable {
+
+    private Long id;
+
+    @ApiModelProperty(value = "社区ID")
+    private Long communityId;
+
+    @ApiModelProperty(value = "房屋ID")
+    private Long houseId;
+
+    @ApiModelProperty(value = "楼栋名")
+    private String building;
+
+    @ApiModelProperty(value = "单元名")
+    private String unit;
+
+    @ApiModelProperty(value = "楼层名")
+    private String floor;
+
+    @ApiModelProperty(value = "门牌名")
+    private String door;
+
+    @ApiModelProperty(value = "社区名称", hidden = true)
+    private String communityName;
+
+}
