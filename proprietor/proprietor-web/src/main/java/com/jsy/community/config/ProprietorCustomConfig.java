bc3559c141c0dd6d606eda8d02fdf47c6308c1dc
Yu Linfeng <yu1650056068@gmail.com>
2020-12-26 10:51:28 +0800

    更新LocalDateTime时间格式化

--- proprietor/proprietor-web/src/main/java/com/jsy/community/config/ProprietorCustomConfig.java
+++ proprietor/proprietor-web/src/main/java/com/jsy/community/config/ProprietorCustomConfig.java
@@ -2,9 +2,15 @@ package com.jsy.community.config;
 
 import com.fasterxml.jackson.annotation.JsonInclude;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
+
+import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 
 /**
  * @author YuLF
@@ -23,10 +29,24 @@ public class ProprietorCustomConfig {
     public MappingJackson2HttpMessageConverter getMappingJackson2HttpMessageConverter() {
         MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter = new MappingJackson2HttpMessageConverter();
         ObjectMapper objectMapper = new ObjectMapper();
+        //处理Date
+        objectMapper.setDateFormat(new SimpleDateFormat("yyyy-MM-dd HH:mm:ss"));
+        // 初始化JavaTimeModule
+        JavaTimeModule javaTimeModule = new JavaTimeModule();
+
+        //处理LocalDateTime
+        DateTimeFormatter dateTimeFormatter = DateTimeFormatter.ofPattern("yyyy-MM-dd HH:mm:ss");
+        javaTimeModule.addSerializer(LocalDateTime.class, new LocalDateTimeSerializer(dateTimeFormatter));
+
+        objectMapper.registerModule(javaTimeModule);
         objectMapper.setSerializationInclusion(JsonInclude.Include.NON_NULL);
         mappingJackson2HttpMessageConverter.setObjectMapper(objectMapper);
         return mappingJackson2HttpMessageConverter;
     }
+
+
+
+
 
 
 }
