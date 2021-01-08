0db57762359b868f5b1c85cb51c3e3895dd7b191
李浩 <javalihao_233@163.com>
2021-01-08 14:49:37 +0800

    商铺图片不再做逻辑删除 采用直接删除

--- common/src/main/java/com/jsy/community/entity/shop/ShopImgEntity.java
+++ common/src/main/java/com/jsy/community/entity/shop/ShopImgEntity.java
@@ -1,12 +1,17 @@
 package com.jsy.community.entity.shop;
 
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
-import com.jsy.community.entity.BaseEntity;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -21,15 +26,23 @@ import lombok.experimental.Accessors;
 @Accessors(chain = true)
 @TableName("t_shop_img")
 @ApiModel(value="ShopImg对象", description="店铺图片")
-public class ShopImgEntity extends BaseEntity {
+public class ShopImgEntity implements Serializable {
 
     private static final long serialVersionUID = 1L;
+    
+    @TableId
+    private Long id;
 
     @ApiModelProperty(value = "商铺id")
     private Long shopId;
 
     @ApiModelProperty(value = "店铺图片地址")
     private String imgUrl;
-
+    
+    @TableField(fill = FieldFill.INSERT)
+    private LocalDateTime createTime;
+    
+    @TableField(fill = FieldFill.UPDATE)
+    private LocalDateTime updateTime;
 
 }
