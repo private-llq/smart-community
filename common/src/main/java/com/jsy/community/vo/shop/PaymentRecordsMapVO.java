14808104cb9320e7feafdb27d49b3540097a0f87
hu-yunfeng <1056134646@qq.com>
2021-01-14 17:16:03 +0800

    生活缴费代码优化--9

--- common/src/main/java/com/jsy/community/vo/shop/PaymentRecordsMapVO.java
+++ common/src/main/java/com/jsy/community/vo/shop/PaymentRecordsMapVO.java
@@ -2,15 +2,16 @@ package com.jsy.community.vo.shop;
 
 import lombok.Data;
 
+import java.io.Serializable;
 import java.util.Map;
 
 /**
  * @program: com.jsy.community
- * @description:
+ * @description: 生活缴费月份账单
  * @author: Hu
  * @create: 2021-01-14 17:11
  **/
 @Data
-public class PaymentRecordsMapVO {
+public class PaymentRecordsMapVO implements Serializable {
     private Map map;
 }
