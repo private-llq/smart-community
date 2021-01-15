b4f845afd246b4d917d41495c5c1d54d0784413b
hu-yunfeng <1056134646@qq.com>
2021-01-15 09:59:47 +0800

    生活缴费代码优化--添加图片上传后缀判断

--- common/src/main/java/com/jsy/community/entity/PayOrderEntity.java
+++ common/src/main/java/com/jsy/community/entity/PayOrderEntity.java
@@ -64,7 +64,7 @@ public class PayOrderEntity extends BaseEntity {
     private Integer billClassification;
 
     @ApiModelProperty(value = "标签")
-    private String label;
+    private String tally;
 
     @ApiModelProperty(value = "备注")
     private String remark;
