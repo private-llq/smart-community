4bf68f4bb2d9db3f1c3915952ff467fcf0d9437b
LLH <javalihao_233@163.com>
2021-01-15 14:38:34 +0800

    商铺租赁添加校验

--- common/src/main/java/com/jsy/community/qo/shop/ShopQO.java
+++ common/src/main/java/com/jsy/community/qo/shop/ShopQO.java
@@ -1,12 +1,13 @@
 package com.jsy.community.qo.shop;
 
+import com.jsy.community.utils.RegexUtils;
 import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
+import org.hibernate.validator.constraints.Length;
+import org.hibernate.validator.constraints.Range;
 
-import javax.validation.constraints.NotBlank;
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Pattern;
+import javax.validation.constraints.*;
 import java.io.Serializable;
 import java.math.BigDecimal;
 import java.util.List;
@@ -37,48 +38,70 @@ public class ShopQO implements Serializable {
 	@NotNull(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class}, message = "商铺地址不能为空")
 	private Long communityId;
 	
+	/**
+	 * 匹配文本： 2层/共18层  或者 共18层 或者 1层至3层/共18层 这样的格式字符串
+	 */
+	@Pattern(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class}, regexp = "^([(-1)-9]{1,2})层/共([0-9]{1,2})层$|^共[1-9]{1,2}层$|^([(-1)-9]{1,2})层至[1-9]{1,2}层/共([0-9]{1,2})层$", message = "请输入正确的楼层文本!")
 	@ApiModelProperty(value = "楼层")
 	@NotBlank(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class}, message = "商铺楼层不能为空")
 	private String floor;
 	
 	@ApiModelProperty(value = "店铺面积  单位：平米")
 	@NotNull(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class}, message = "建筑面积不能为空")
+	@DecimalMin(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class},value = "1.00",message = "面积最少为1.00")
+	@DecimalMax(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class},value = "99999.99",message = "面积最大为99999.99")
 	private Double shopAcreage;
 	
 	@ApiModelProperty(value = "面宽")
 	@NotNull(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class}, message = "面宽不能为空")
+	@DecimalMin(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class},value = "1.00",message = "面宽最少为1.00")
+	@DecimalMax(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class},value = "999.99",message = "面宽最大为999.99")
 	private Double shopWidth;
 	
 	@ApiModelProperty(value = "进深")
 	@NotNull(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class}, message = "进深不能为空")
+	@DecimalMin(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class},value = "1.00",message = "进深最少为1.00")
+	@DecimalMax(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class},value = "999.99",message = "进深最大为999.99")
 	private Double shopDepth;
 	
 	@ApiModelProperty(value = "层高")
 	@NotNull(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class}, message = "层高不能为空")
+	@DecimalMin(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class},value = "1.00",message = "进深最少为1.00")
+	@DecimalMax(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class},value = "999.99",message = "进深最大为99.99")
 	private Double shopHeight;
 	
 	@ApiModelProperty(value = "标题")
 	@NotBlank(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class}, message = "标题不能为空")
+	@Length(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class},min = 1,max = 20,message = "标题最大长度为20个字符")
 	private String title;
 	
 	@ApiModelProperty(value = "概述")
 	@NotBlank(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class}, message = "概述不能为空")
+	@Length(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class},min = 1,max = 500,message = "概述最大长度为500个字符")
 	private String summarize;
 	
 	@ApiModelProperty(value = "月租金")
 	@NotNull(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class}, message = "月租金不能为空")
+	@DecimalMin(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class},value = "1.00",message = "月租金最少为1.00")
+	@DecimalMax(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class},value = "99999.99",message = "月租金最大为99999.99")
 	private BigDecimal monthMoney;
 	
 	@ApiModelProperty(value = "转让费")
 	@NotNull(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class}, message = "转让费不能为空")
+	@DecimalMin(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class},value = "1.00",message = "转让费最少为1.00")
+	@DecimalMax(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class},value = "9999999.99",message = "转让费最大为9999999.99")
 	private BigDecimal transferMoney;
 	
 	@ApiModelProperty(value = "免租月数")
 	@NotNull(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class}, message = "免租月数不能为空")
+	@DecimalMin(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class},value = "0.00",message = "免租月数最少为0.00")
+	@DecimalMax(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class},value = "99.00",message = "免租月数最大为99")
 	private Integer freeLease;
 	
 	@ApiModelProperty(value = "起租月数")
 	@NotNull(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class}, message = "起租月数不能为空")
+	@DecimalMin(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class},value = "0.00",message = "起租月数最少为0.00")
+	@DecimalMax(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class},value = "99.00",message = "起租月数最大为99")
 	private Integer startLease;
 	
 	@ApiModelProperty(value = "押付方式")
@@ -88,6 +111,7 @@ public class ShopQO implements Serializable {
 	
 	@ApiModelProperty(value = "店铺状态 0空置中 1营业中")
 	@NotNull(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class}, message = "商铺状态不能为空")
+	@Range(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class},min = 0,max = 1,message = "店铺状态只能是空置中或营业中")
 	private Integer status;
 	
 	@ApiModelProperty(value = "经度")
@@ -98,16 +122,19 @@ public class ShopQO implements Serializable {
 	@NotNull(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class}, message = "纬度不能为空")
 	private BigDecimal lat;
 	
-	@ApiModelProperty(value = "来源 0个人 1物业")
+	@ApiModelProperty(value = "来源 1业主 2物业")
 	@NotNull(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class}, message = "来源不能为空")
+	@Range(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class},min = 0,max = 1,message = "店铺来源只能是业主或物业")
 	private Integer source;
 	
 	@ApiModelProperty(value = "称呼")
 	@NotBlank(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class}, message = "称呼不能为空")
+	@Length(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class},min = 1,max = 10,message = "称呼最大长度为10个字符")
 	private String nickname;
 	
 	@ApiModelProperty(value = "手机")
 	@NotBlank(groups = {ShopQO.addShopValidate.class, ShopQO.updateShopValidate.class}, message = "手机号不能为空")
+	@Pattern(groups = {ShopQO.addShopValidate.class},regexp = RegexUtils.REGEX_MOBILE,message = "您输入的联系电话不符合要求")
 	private String mobile;
 	
 	@ApiModelProperty(value = "图片地址集合")
