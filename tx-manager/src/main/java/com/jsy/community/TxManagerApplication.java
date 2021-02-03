4dfcd9217104e3dc32634f1beca867be10c1a71b
Yu Linfeng <yu1650056068@gmail.com>
2021-02-03 16:48:14 +0800

    事务模块排除 所有模块都需要加载的RedisConfig

--- tx-manager/src/main/java/com/jsy/community/TxManagerApplication.java
+++ tx-manager/src/main/java/com/jsy/community/TxManagerApplication.java
@@ -1,11 +1,16 @@
 package com.jsy.community;
 
 import com.codingapi.txlcn.tm.config.EnableTransactionManagerServer;
+import com.jsy.community.config.service.RedisConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.FilterType;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
 @EnableTransactionManagerServer
+@ComponentScan(value = "com.jsy.community", excludeFilters = @ComponentScan.Filter(type = FilterType.ASSIGNABLE_TYPE, classes = {RedisConfig.class}))
 public class TxManagerApplication {
 
 	public static void main(String[] args) {
