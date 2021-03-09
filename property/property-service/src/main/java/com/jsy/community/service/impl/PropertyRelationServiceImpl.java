7f01e24ddd77ba50559eb59998b9a627ee1a1036
hu-yunfeng <1056134646@qq.com>
2021-03-09 14:18:15 +0800

    物业后台管理成员查询

--- property/property-service/src/main/java/com/jsy/community/service/impl/PropertyRelationServiceImpl.java
+++ property/property-service/src/main/java/com/jsy/community/service/impl/PropertyRelationServiceImpl.java
@@ -3,8 +3,12 @@ package com.jsy.community.service.impl;
 import com.jsy.community.api.IPropertyRelationService;
 import com.jsy.community.constant.Const;
 import com.jsy.community.mapper.PropertyRelationMapper;
+import com.jsy.community.qo.BaseQO;
+import com.jsy.community.qo.property.PropertyRelationQO;
 import org.apache.dubbo.config.annotation.DubboService;
 import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
 
 /**
  * @program: com.jsy.community
@@ -17,4 +21,9 @@ public class PropertyRelationServiceImpl implements IPropertyRelationService {
     @Autowired
     private PropertyRelationMapper propertyRelationMapper;
 
+
+    @Override
+    public List list(BaseQO<PropertyRelationQO> baseQO) {
+        return null;
+    }
 }
--- property/property-web/src/main/java/com/jsy/community/controller/PropertyRelationController.java
+++ property/property-web/src/main/java/com/jsy/community/controller/PropertyRelationController.java
@@ -3,10 +3,17 @@ package com.jsy.community.controller;
 import com.jsy.community.annotation.ApiJSYController;
 import com.jsy.community.api.IPropertyRelationService;
 import com.jsy.community.constant.Const;
+import com.jsy.community.qo.BaseQO;
+import com.jsy.community.qo.property.PropertyRelationQO;
+import com.jsy.community.vo.CommonResult;
 import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 /**
  * @program: com.jsy.community
@@ -23,10 +30,10 @@ public class PropertyRelationController {
     @DubboReference(version = Const.version, group = Const.group, check = false)
     private IPropertyRelationService propertyRelationService;
 
-//    @ApiOperation("查询业主下所有成员")
-//    @PostMapping("/list")
-//    public CommonResult list(@RequestBody BaseQO baseQO){
-//        List list=propertyRelationService.list();
-//        return CommonResult.ok(list);
-//    }
+    @ApiOperation("查询业主下所有成员")
+    @PostMapping("/list")
+    public CommonResult list(BaseQO<PropertyRelationQO> baseQO){
+        List list=propertyRelationService.list(baseQO);
+        return CommonResult.ok();
+    }
 }
