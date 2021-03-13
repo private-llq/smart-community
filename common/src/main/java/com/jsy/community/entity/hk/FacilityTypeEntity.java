850383b5b349d0881080c305477fa04796d9de2a
李浩 <javalihao_233@163.com>
2021-03-13 13:59:28 +0800

    物业端设备分类完成

---
+++ common/src/main/java/com/jsy/community/entity/hk/FacilityTypeEntity.java
@@ -0,0 +1,47 @@
+package com.jsy.community.entity.hk;
+
+import com.baomidou.mybatisplus.annotation.TableName;
+import com.jsy.community.entity.BaseEntity;
+import io.swagger.annotations.ApiModel;
+import io.swagger.annotations.ApiModelProperty;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+
+/**
+ * <p>
+ * 
+ * </p>
+ *
+ * @author lihao
+ * @since 2021-03-12
+ */
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@TableName("t_facility_type")
+@ApiModel(value="FacilityType对象", description="设备分类信息")
+public class FacilityTypeEntity extends BaseEntity {
+
+    @ApiModelProperty(value = "父id")
+    @NotNull(groups = {FacilityTypeEntity.addFacilityValidate.class},message = "父id不能为空")
+    private Long pid;
+
+    @ApiModelProperty(value = "社区id")
+    @NotNull(groups = {FacilityTypeEntity.addFacilityValidate.class, FacilityTypeEntity.updateFacilityValidate.class},message = "社区id不能为空")
+    private Long communityId;
+
+    @ApiModelProperty(value = "类别名称")
+    @NotBlank(groups = {FacilityTypeEntity.addFacilityValidate.class, FacilityTypeEntity.updateFacilityValidate.class},message = "类别名称不能为空")
+    private String name;
+
+    public interface addFacilityValidate {
+    }
+    
+    public interface updateFacilityValidate {
+    }
+
+}
