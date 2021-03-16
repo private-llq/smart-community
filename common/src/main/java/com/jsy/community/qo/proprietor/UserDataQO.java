ee730ef924ccb5bad9acc7295f70a3ff244e841d
hu-yunfeng <1056134646@qq.com>
2021-03-16 10:49:07 +0800

    用户个人信息优化

--- common/src/main/java/com/jsy/community/qo/proprietor/UserDataQO.java
+++ common/src/main/java/com/jsy/community/qo/proprietor/UserDataQO.java
@@ -1,5 +1,6 @@
 package com.jsy.community.qo.proprietor;
 
+import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
 import java.io.Serializable;
@@ -7,6 +8,7 @@ import java.time.LocalDate;
 
 @Data
 public class UserDataQO implements Serializable {
+    @ApiModelProperty(hidden = true)
     private String avatarUrl;
     private String nickname;
     private LocalDate birthdayTime;
