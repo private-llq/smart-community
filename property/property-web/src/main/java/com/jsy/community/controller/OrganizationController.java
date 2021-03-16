2750a3a7012db6e9af748b2d8824d36c09004de8
李浩 <javalihao_233@163.com>
2021-03-16 10:11:02 +0800

    完成物业端组织机构功能代码

---
+++ property/property-web/src/main/java/com/jsy/community/controller/OrganizationController.java
@@ -0,0 +1,81 @@
+package com.jsy.community.controller;
+
+
+import com.jsy.community.annotation.ApiJSYController;
+import com.jsy.community.api.IOrganizationService;
+import com.jsy.community.api.PropertyException;
+import com.jsy.community.constant.Const;
+import com.jsy.community.entity.OrganizationEntity;
+import com.jsy.community.utils.ValidatorUtils;
+import com.jsy.community.vo.CommonResult;
+import com.jsy.community.vo.TreeCommunityVO;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiParam;
+import org.apache.dubbo.config.annotation.DubboReference;
+import org.springframework.web.bind.annotation.*;
+
+/**
+ * <p>
+ * 前端控制器
+ * </p>
+ *
+ * @author lihao
+ * @since 2021-03-15
+ */
+@Api(tags = "组织机构控制器")
+@RestController
+@ApiJSYController
+@RequestMapping("/organization")
+public class OrganizationController {
+	
+	@DubboReference(version = Const.version, group = Const.group_property, check = false)
+	private IOrganizationService organizationService;
+	
+	@ApiOperation("树形查询所有组织")
+	@GetMapping("/listOrganization")
+	public CommonResult<TreeCommunityVO> listOrganization(@ApiParam("社区id") @RequestParam Long communityId) {
+		TreeCommunityVO treeCommunityVO = organizationService.listOrganization(communityId);
+		return CommonResult.ok(treeCommunityVO);
+	}
+	
+	@ApiOperation("新增组织机构")
+	@PostMapping("/addOrganization")
+	public CommonResult addOrganization(@RequestBody OrganizationEntity organizationEntity) {
+		ValidatorUtils.validateEntity(organizationEntity, OrganizationEntity.addOrganizationValidate.class);
+		if (organizationEntity.getSort() > 99 || organizationEntity.getSort() < 0) {
+			throw new PropertyException("你输入的排序序号不符要求,请重新输入!");
+		}
+		organizationService.addOrganization(organizationEntity);
+		return CommonResult.ok();
+	}
+	
+	@ApiOperation("删除组织机构")
+	@GetMapping("/deleteOrganization")
+	public CommonResult deleteOrganization(Long id, Long communityId) {
+		organizationService.deleteOrganization(id, communityId);
+		return CommonResult.ok();
+	}
+	
+	@ApiOperation("根据id查询组织机构")
+	@GetMapping("/getOrganizationById")
+	public CommonResult getOrganizationById(Long id, Long communityId) {
+		OrganizationEntity organization = organizationService.getOrganizationById(id, communityId);
+		return CommonResult.ok(organization);
+	}
+	
+	
+	@ApiOperation("修改组织机构")
+	@GetMapping("/updateOrganization")
+	public CommonResult updateOrganization(OrganizationEntity organization) {
+		ValidatorUtils.validateEntity(organization, OrganizationEntity.addOrganizationValidate.class);
+		if (organization.getSort() > 99 || organization.getSort() < 0) {
+			throw new PropertyException("你输入的排序序号不符要求,请重新输入!");
+		}
+		organizationService.updateOrganization(organization);
+		return CommonResult.ok();
+	}
+	
+	
+}
+
