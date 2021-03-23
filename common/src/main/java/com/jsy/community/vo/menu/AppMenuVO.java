95863afeb706fbc634df13a8b3e743e198725a3e
李浩 <javalihao_233@163.com>
2021-03-23 15:55:37 +0800

    菜单修改(漏)

---
+++ common/src/main/java/com/jsy/community/vo/menu/AppMenuVO.java
@@ -0,0 +1,27 @@
+package com.jsy.community.vo.menu;
+
+import io.swagger.annotations.ApiModel;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.experimental.Accessors;
+
+import java.io.Serializable;
+
+/**
+ * @author lihao
+ * @ClassName AppMenuVO
+ * @Date 2021/3/23  15:15
+ * @Description TODO
+ * @Version 1.0
+ **/
+@Data
+@EqualsAndHashCode(callSuper = false)
+@Accessors(chain = true)
+@ApiModel(value = "AppMenuVO对象", description = "物业端菜单对象")
+public class AppMenuVO implements Serializable {
+	private Long communityId;
+	
+	private Long menuId;
+	
+	private Integer sort;
+}
