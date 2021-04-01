7071871b00abfafaec25a5cdc6f33c3b684ebe5f
qq459799974 <459799974@qq.com>
2021-04-01 14:13:29 +0800

    增加一个公共模块

---
+++ property/property-common/src/main/java/com/jsy/community/consts/PropertyConsts.java
@@ -0,0 +1,13 @@
+package com.jsy.community.consts;
+
+/**
+ * @author chq459799974
+ * @description 物业端常量
+ * @since 2021-04-01 13:44
+ **/
+public interface PropertyConsts {
+	/**
+	 * 超级管理员
+	 */
+	int SUPER_ADMIN = 1;
+}
