d6cd652ea90b2092c44d174e31b5f06845a422a0
qq459799974 <459799974@qq.com>
2021-04-20 18:11:58 +0800

    物业端社区资料查询接口假数据补充

--- property/property-web/src/main/java/com/jsy/community/controller/PropertyAccountController.java
+++ property/property-web/src/main/java/com/jsy/community/controller/PropertyAccountController.java
@@ -4,6 +4,7 @@ import com.jsy.community.annotation.ApiJSYController;
 import com.jsy.community.annotation.auth.Login;
 import com.jsy.community.api.IPropertyAccountService;
 import com.jsy.community.constant.Const;
+import com.jsy.community.entity.property.PropertyAccountBankEntity;
 import com.jsy.community.utils.UserUtils;
 import com.jsy.community.vo.CommonResult;
 import io.swagger.annotations.Api;
@@ -13,12 +14,15 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author chq459799974
  * @description 社区对公账户Controller
  * @since 2021-04-20 17:51
  **/
-@Api(tags = "社区对公账户")
+@Api(tags = "社区账户信息")
 @RestController
 @RequestMapping("/account")
 @ApiJSYController
@@ -29,16 +33,35 @@ public class PropertyAccountController {
 	private IPropertyAccountService propertyAccountService;
 	
 	/**
-	* @Description: 查询本社区对公账户
+	* @Description: 本社区社区信息查询
 	 * @Param: []
 	 * @Return: com.jsy.community.vo.CommonResult
 	 * @Author: chq459799974
 	 * @Date: 2021/4/20
 	**/
-	@ApiOperation("查询本社区对公账户")
+	
+	@ApiOperation("本社区社区信息查询")
 	@GetMapping("")
 	public CommonResult queryByCommunityId(){
-		return CommonResult.ok(propertyAccountService.queryBankAccount(UserUtils.getAdminCommunityId()),"查询成功");
+		Map<String, Object> returnMap = new HashMap<>();
+		//对公账户
+		PropertyAccountBankEntity account = propertyAccountService.queryBankAccount(UserUtils.getAdminCommunityId());
+		returnMap.put("account",account);
+		
+		//企业信息
+		Map<String, Object> companyInfoMap = new HashMap<>();
+		companyInfoMap.put("companyName","重庆纵横世纪科技有限公司");
+		companyInfoMap.put("creditCode","J11233332322223321");
+		companyInfoMap.put("legalPerson","张三");
+		
+		//结算方式
+		Map<String, Object> statementMap = new HashMap<>();
+		statementMap.put("content","每月5~7号自动结算上月费用，如遇法定节假日将顺延");
+		
+		//组装返回
+		returnMap.put("companyInfo",companyInfoMap);
+		returnMap.put("statement",statementMap);
+		return CommonResult.ok(returnMap,"查询成功");
 	}
 	
 }
