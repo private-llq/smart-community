c6f628c9cf48b8d1c650957987ff80f18a5d7bb1
qq459799974 <459799974@qq.com>
2021-04-23 14:05:28 +0800

    物业财务-结算单批量映射查询

--- property/property-api/src/main/java/com/jsy/community/api/IPropertyFinanceStatementService.java
+++ property/property-api/src/main/java/com/jsy/community/api/IPropertyFinanceStatementService.java
@@ -3,6 +3,9 @@ package com.jsy.community.api;
 import com.baomidou.mybatisplus.extension.service.IService;
 import com.jsy.community.entity.property.PropertyFinanceStatementEntity;
 
+import java.util.Collection;
+import java.util.Map;
+
 /**
  * @Author: Pipi
  * @Description: 物业财务-结算单服务
@@ -18,4 +21,13 @@ public interface IPropertyFinanceStatementService extends IService<PropertyFinan
      *@Date: 2021/4/22 16:59
      **/
     void timingStatement();
+    
+    /**
+    * @Description: 结算单号批量查 单号-结算单数据 映射
+     * @Param: [nums]
+     * @Return: java.util.Map<java.lang.String,com.jsy.community.entity.property.PropertyFinanceStatementEntity>
+     * @Author: chq459799974
+     * @Date: 2021/4/23
+    **/
+    Map<String,PropertyFinanceStatementEntity> queryByStatementNumBatch(Collection<String> nums);
 }
