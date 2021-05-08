14a677bc071743e21e6e8eec69443e817ae61c26
李浩 <javalihao_233@163.com>
2021-05-08 17:46:36 +0800

    测试111

--- proprietor/proprietor-common/src/main/java/com/jsy/community/consts/ProprietorConstsEnum.java
+++ proprietor/proprietor-common/src/main/java/com/jsy/community/consts/ProprietorConstsEnum.java
@@ -1,10 +1,5 @@
 package com.jsy.community.consts;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-
 /**
  * @author chq459799974
  * @description 业主端枚举
