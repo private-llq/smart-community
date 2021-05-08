034c9851e28b21cedd1e9157302073f816898526
李浩 <javalihao_233@163.com>
2021-05-08 17:45:18 +0800

    测试111

---
+++ proprietor/proprietor-common/src/main/java/com/jsy/community/consts/ProprietorConsts.java
@@ -0,0 +1,15 @@
+package com.jsy.community.consts;
+
+/**
+ * @author chq459799974
+ * @description 业主端常量
+ * @since 2021-05-08 13:44
+ **/
+public interface ProprietorConsts {
+	//======= 是否展示系统消息
+	Integer COMMUNITY_CONFIG_SHOW_SYS_MSG = 1; //展示
+	Integer COMMUNITY_CONFIG_NOT_SHOW_SYS_MSG = 0; //不展示
+	//======= 是否展示系统广告(轮播图)
+	Integer COMMUNITY_CONFIG_SHOW_SYS_BANNER = 1; //展示
+	Integer COMMUNITY_CONFIG_NOT_SHOW_SYS_BANNER = 0; //不展示
+}
