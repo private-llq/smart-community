2a97c685953d5630b286eba178a110e321b35133
qq459799974 <459799974@qq.com>
2021-05-27 13:42:20 +0800

    还原

--- facility/facility-service/src/main/java/com/jsy/community/callback/FRealDataCallBack.java
+++ facility/facility-service/src/main/java/com/jsy/community/callback/FRealDataCallBack.java
@@ -1,17 +1,11 @@
 package com.jsy.community.callback;
 
 import com.jsy.community.sdk.HCNetSDK;
-import com.jsy.community.websocket.CommunityWebSocketHandler;
 import com.sun.jna.NativeLong;
 import com.sun.jna.Pointer;
 import com.sun.jna.ptr.ByteByReference;
 import org.springframework.stereotype.Component;
-import org.springframework.web.socket.BinaryMessage;
-import org.springframework.web.socket.WebSocketSession;
 
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
 
 /**
  * @return
@@ -23,22 +17,9 @@ import java.util.Map;
 @Component
 public class FRealDataCallBack implements HCNetSDK.FRealDataCallBack_V30 {
 	
-	//TODO 定时批量把这个对象格式化输出到文件
-	private static Map ERROR_MSG_MAP = new HashMap();
-	
 	@Override
 	public void invoke(NativeLong lRealHandle, int dwDataType, ByteByReference pBuffer, int dwBufSize, Pointer pUser) {
-		WebSocketSession webSocketSession = CommunityWebSocketHandler.SOCKET_SESSION_MAP.get("AdminUserId");
-		if(webSocketSession != null){
-//			System.out.println(new String(bytes));
-			try {
-//				int i=1/0;
-				byte[] bytes = pBuffer.getPointer().getByteArray(0, dwBufSize);
-				webSocketSession.sendMessage(new BinaryMessage(bytes));
-			} catch (Exception e) {
-				ERROR_MSG_MAP.put(e.getClass().getSimpleName(),e.getMessage());
-			}
-		}
+//		byte[] bytes = pBuffer.getPointer().getByteArray(0, dwBufSize);
 		// 在回调函数里面写业务逻辑
 	}
 }
