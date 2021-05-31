21e8b24543df2b06a5adee94a5bd427944a00ac0
qq459799974 <459799974@qq.com>
2021-05-31 15:30:51 +0800

    APP版本查询接口

---
+++ proprietor/proprietor-service/src/main/java/com/jsy/community/service/impl/AppVersionServiceImpl.java
@@ -0,0 +1,46 @@
+package com.jsy.community.service.impl;
+
+import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
+import com.jsy.community.api.IAppVersionService;
+import com.jsy.community.constant.BusinessConst;
+import com.jsy.community.constant.Const;
+import com.jsy.community.entity.AppVersionEntity;
+import com.jsy.community.mapper.AppVersionMapper;
+import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.StringUtils;
+
+import java.util.List;
+
+/**
+ * @author chq459799974
+ * @description APP版本查询Service实现类
+ * @since 2021-05-31 13:57
+ **/
+@DubboService(version = Const.version, group = Const.group_proprietor)
+public class AppVersionServiceImpl implements IAppVersionService {
+	
+	@Autowired
+	private AppVersionMapper aPPVersionMapper;
+	
+	/**
+	* @Description: 查询APP版本列表 1.安卓 2.IOS
+	 * @Param: [sysType,sysVersion]
+	 * @Return: java.util.List<com.jsy.community.entity.AppVersionEntity>
+	 * @Author: chq459799974
+	 * @Date: 2021/5/31
+	**/
+	@Override
+	public List<AppVersionEntity> queryAppVersionList(Integer sysType,String sysVersion){
+		QueryWrapper queryWrapper = new QueryWrapper<>();
+		queryWrapper.select("*");
+		if(sysType != null){
+			queryWrapper.eq("sys_type",sysType);
+		}
+		if(!StringUtils.isEmpty(sysVersion)){
+			queryWrapper.eq("sys_version",sysVersion);
+		}
+		queryWrapper.orderByDesc("create_time");
+		return aPPVersionMapper.selectList(queryWrapper);
+	}
+}
