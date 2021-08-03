90dad43cc04b4052fd7eec900fe649719634c999
qq459799974 <459799974@qq.com>
2021-08-03 13:59:28 +0800

    云端访客同步小区部分代码

--- property/property-service/src/main/java/com/jsy/community/mapper/VisitorHistoryMapper.java
+++ property/property-service/src/main/java/com/jsy/community/mapper/VisitorHistoryMapper.java
@@ -5,6 +5,8 @@ import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
 import com.jsy.community.entity.VisitorHistoryEntity;
 import org.apache.ibatis.annotations.Param;
 
+import java.util.Collection;
+
 
 /**
  * @author chq459799974
@@ -12,7 +14,14 @@ import org.apache.ibatis.annotations.Param;
  * @since 2021-04-13 13:44
  **/
 public interface VisitorHistoryMapper extends BaseMapper<VisitorHistoryEntity> {
-
+	
+	/**
+	 * 分页查询
+	 */
 	Page<VisitorHistoryEntity> queryPage(Page<VisitorHistoryEntity> page, @Param("query") VisitorHistoryEntity query);
 	
+	/**
+	 * 批量新增
+	 */
+	int insertBatch(@Param("dataList")Collection dataList);
 }
