a39e8b053cf1ce98425e2e5f0652421f42916b8d
saam <446523904@qq.com>
2021-08-09 14:23:00 +0800

    增加导出房屋信息表接口,查询楼栋、单元、房屋接口增加批量查询楼宇分类名称

--- property/property-service/src/main/java/com/jsy/community/mapper/HouseBuildingTypeMapper.java
+++ property/property-service/src/main/java/com/jsy/community/mapper/HouseBuildingTypeMapper.java
@@ -2,6 +2,11 @@ package com.jsy.community.mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.jsy.community.entity.HouseBuildingTypeEntity;
+import org.apache.ibatis.annotations.MapKey;
+import org.apache.ibatis.annotations.Param;
+
+import java.util.List;
+import java.util.Map;
 
 /**
  * <p>
@@ -12,5 +17,15 @@ import com.jsy.community.entity.HouseBuildingTypeEntity;
  * @since 2021-08-05
  */
 public interface HouseBuildingTypeMapper extends BaseMapper<HouseBuildingTypeEntity> {
-
+	
+	/**
+	 * @Description: 批量查询楼宇分类名称
+	 * @Param: [ids]
+	 * @Return: java.util.Map<java.lang.Integer,java.util.Map<java.lang.String,java.lang.String>>
+	 * @Author: DKS
+	 * @Date: 2021/8/9
+	 **/
+	@MapKey("id")
+	Map<Integer, Map<String,String>> queryHouseBuildingType(@Param("list") List<Integer> ids);
+	
 }
