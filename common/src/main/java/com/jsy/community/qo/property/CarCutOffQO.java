2362ddc8a75096527b12469ad26265f21d626abe
Administrator <1973982151@qq.com>
2021-09-03 15:22:17 +0800

    车辆所属类型

--- common/src/main/java/com/jsy/community/qo/property/CarCutOffQO.java
+++ common/src/main/java/com/jsy/community/qo/property/CarCutOffQO.java
@@ -23,6 +23,9 @@ public class CarCutOffQO extends BaseQO implements Serializable {
 //    @ApiModelProperty(value = "进出方向")
 //    private String access;
 
+    @ApiModelProperty(value = "车辆所属类型  1-临时 2-包月  3-业主")
+    private Integer belong;
+
     @ApiModelProperty(value = "0为未完成 在场车辆   1为完成 进出车辆")
     private Integer state;
 
