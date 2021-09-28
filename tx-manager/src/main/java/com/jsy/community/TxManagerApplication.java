86b40e518401c70157b54aa693e83bffb650a025
tnz <tannianzhou123456@163.com>
2021-09-28 09:53:23 +0800

    tnz

--- tx-manager/src/main/java/com/jsy/community/TxManagerApplication.java
+++ tx-manager/src/main/java/com/jsy/community/TxManagerApplication.java
@@ -4,6 +4,7 @@ import com.codingapi.txlcn.tm.config.EnableTransactionManagerServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.boot.context.ApplicationPidFileWriter;
 
 
 @SpringBootApplication
@@ -14,6 +15,10 @@ public class TxManagerApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TxManagerApplication.class, args);
+
+//		SpringApplication app = new SpringApplication(TxManagerApplication.class);
+//		app.addListeners(new ApplicationPidFileWriter("app.pid"));
+//		app.run(args);
 	}
 
 }
