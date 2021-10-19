6cdb9815ac423bcc163a6d9cfaaf7ba7702993cc
Administrator <1973982151@qq.com>
2021-10-19 14:41:33 +0800

    业主车辆

--- property/property-service/src/main/java/com/jsy/community/service/impl/CarProprietorServiceImpl.java
+++ property/property-service/src/main/java/com/jsy/community/service/impl/CarProprietorServiceImpl.java
@@ -49,11 +49,6 @@ public class CarProprietorServiceImpl extends ServiceImpl<CarProprietorMapper,Ca
     public Page<CarProprietorEntity> listPage(CarProprietorEntity baseQO, Long adminCommunityId) {
 
         Page<CarProprietorEntity> page = new Page<CarProprietorEntity>(baseQO.getPage(), baseQO.getSize());
-
-//        CarProprietorEntity query = baseQO.getQuery();
-//        query.setCommunityId(adminCommunityId);
-        System.out.println(baseQO.getPage());
-        System.out.println(baseQO.getSize());
         QueryWrapper<CarProprietorEntity> queryWrapper = new QueryWrapper<CarProprietorEntity>().eq("community_id", adminCommunityId);
         if (baseQO.getQuery().getPhone()!=null){
             queryWrapper.like("phone",baseQO.getQuery().getPhone());
