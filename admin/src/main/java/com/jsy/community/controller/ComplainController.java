aa3dce450f03a2a85cdaa82a7328fb0b65266d05
saam <446523904@qq.com>
2021-10-27 18:06:40 +0800

    增加意见反馈模块

---
+++ admin/src/main/java/com/jsy/community/controller/ComplainController.java
@@ -0,0 +1,72 @@
+package com.jsy.community.controller;
+
+import com.jsy.community.annotation.ApiJSYController;
+import com.jsy.community.annotation.auth.Login;
+import com.jsy.community.entity.ComplainEntity;
+import com.jsy.community.exception.JSYError;
+import com.jsy.community.qo.BaseQO;
+import com.jsy.community.qo.proprietor.ComplainQO;
+import com.jsy.community.service.IComplainService;
+import com.jsy.community.utils.PageInfo;
+import com.jsy.community.vo.CommonResult;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.springframework.web.bind.annotation.*;
+
+import javax.annotation.Resource;
+
+/**
+ * @program: com.jsy.community
+ * @description:  物业意见反馈
+ * @author: DKS
+ * @create: 2021-10-27 15:58
+ **/
+@Api(tags = "物业意见反馈")
+@RestController
+@RequestMapping("/complain")
+@ApiJSYController
+public class ComplainController {
+
+    @Resource
+    private IComplainService complainService;
+    
+    /**
+     * @Description: 意见反馈条件查询
+     * @Param: [baseQO]
+     * @Return: com.jsy.community.vo.CommonResult<com.jsy.community.utils.PageInfo<com.jsy.community.entity.ComplainEntity>>
+     * @Author: DKS
+     * @Date: 2021/10/27
+     **/
+    @Login
+    @ApiOperation("意见反馈条件查询")
+    @PostMapping("/query")
+    public CommonResult<PageInfo<ComplainEntity>> queryComplain(@RequestBody BaseQO<ComplainQO> baseQO){
+        return CommonResult.ok(complainService.queryComplain(baseQO));
+    }
+    
+    /**
+     * @Description: 意见反馈详情查询
+     * @Param: [baseQO]
+     * @Return: com.jsy.community.vo.CommonResult
+     * @Author: DKS
+     * @Date: 2021/10/19
+     **/
+    @Login
+    @GetMapping("/getDetail")
+    public CommonResult getDetailComplain(Long id){
+        return CommonResult.ok(complainService.getDetailComplain(id));
+    }
+    
+    /**
+     * @Description: 意见反馈删除
+     * @Param: [baseQO]
+     * @Return: com.jsy.community.vo.CommonResult
+     * @Author: DKS
+     * @Date: 2021/10/27
+     **/
+    @Login
+    @DeleteMapping("/delete")
+    public CommonResult deleteComplain(Long id){
+        return complainService.deleteComplain(id)? CommonResult.ok() : CommonResult.error(JSYError.INTERNAL.getCode(),"删除失败");
+    }
+}
---
+++ admin/src/main/java/com/jsy/community/mapper/ComplainMapper.java
@@ -0,0 +1,16 @@
+package com.jsy.community.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.jsy.community.entity.ComplainEntity;
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @program: com.jsy.community
+ * @description:  物业意见反馈
+ * @author: Hu
+ * @create: 2021-04-11 11:16
+ **/
+@Mapper
+public interface ComplainMapper extends BaseMapper<ComplainEntity> {
+
+}
