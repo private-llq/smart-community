d1130bad259f4d90935c7c87f561cae32c593ca6
saam <446523904@qq.com>
2021-10-27 15:22:58 +0800

    增加新增消息通知接口，增加删除消息通知接口，增加查询消息通知详情接口

--- admin/src/main/java/com/jsy/community/controller/SysInformController.java
+++ admin/src/main/java/com/jsy/community/controller/SysInformController.java
@@ -8,7 +8,9 @@ import com.jsy.community.exception.JSYError;
 import com.jsy.community.exception.JSYException;
 import com.jsy.community.qo.BaseQO;
 import com.jsy.community.qo.proprietor.OldPushInformQO;
+import com.jsy.community.qo.proprietor.PushInformQO;
 import com.jsy.community.service.ISysInformService;
+import com.jsy.community.utils.UserUtils;
 import com.jsy.community.utils.ValidatorUtils;
 import com.jsy.community.vo.CommonResult;
 import io.swagger.annotations.Api;
@@ -18,6 +20,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.*;
 
+import javax.annotation.Resource;
 import java.util.List;
 
 
@@ -31,9 +34,9 @@ import java.util.List;
 @RequestMapping("sys/inform")
 @ApiJSYController
 public class SysInformController {
-
-
-    private final ISysInformService iSysInformService;
+    
+    @Resource
+    private ISysInformService iSysInformService;
 
     @Autowired
     public SysInformController(ISysInformService iSysInformService) {
@@ -90,5 +93,52 @@ public class SysInformController {
         qo.getQuery().setId(sysInformId);
         return CommonResult.ok(iSysInformService.query(qo));
     }
+    
+    /**
+     * (物业端)新增推送通知消息
+     * @param qo 新增推送消息
+     * @return 返回是否新增成功
+     */
+    @Login
+    @PostMapping("/add")
+    @ApiOperation("添加推送通知消息")
+    @businessLog(operation = "新增",content = "新增了【推送通知消息】")
+    public CommonResult<Boolean> addPushInform(@RequestBody PushInformQO qo) {
+        qo.setPushTarget(1);
+        if (qo.getPushTag() == null) {
+            // 默认关闭推送
+            qo.setPushTag(0);
+        }
+        ValidatorUtils.validateEntity(qo);
+        qo.setUid(UserUtils.getId());
+        return iSysInformService.addPushInform(qo) ? CommonResult.ok("添加成功!") : CommonResult.error("添加失败!");
+    }
+	
+	/**
+	 * (物业端)删除通知消息 [管理员]
+	 * @param id 消息id
+	 * @return 返回修改成功值
+	 */
+	@Login
+	@DeleteMapping("/delete")
+	@ApiOperation("删除推送通知消息")
+	@businessLog(operation = "删除",content = "删除了【推送通知消息】")
+	public CommonResult<Boolean> deletePushInform(@RequestParam Long id) {
+		return iSysInformService.deletePushInform(id, UserUtils.getId()) ? CommonResult.ok("删除成功!") : CommonResult.error(JSYError.NOT_IMPLEMENTED);
+	}
+	
+	/**
+	 *@Author: DKS
+	 *@Description:  获取单条消息详情
+	 *@Param: id: 消息ID
+	 *@Return: com.jsy.community.vo.CommonResult<?>
+	 *@Date: 2021/10/27 14:30
+	 **/
+	@Login
+	@GetMapping("/getDetatil")
+	@ApiOperation("获取单条消息详情")
+	public CommonResult<?> getDetatil(@RequestParam Long id) {
+		return CommonResult.ok(iSysInformService.getDetail(id));
+	}
 
 }
