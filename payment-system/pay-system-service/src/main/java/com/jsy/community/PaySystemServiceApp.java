dd50d7d38621776d3299b103a5b4b5a5c7fdd143
ymx512422598@163.com <ymx512422598@163.com>
2021-11-04 14:32:13 +0800

    继续修改配置

--- payment-system/pay-system-service/src/main/java/com/jsy/community/PaySystemServiceApp.java
+++ payment-system/pay-system-service/src/main/java/com/jsy/community/PaySystemServiceApp.java
@@ -1,5 +1,6 @@
 package com.jsy.community;
 
+import com.codingapi.txlcn.tc.config.EnableDistributedTransaction;
 import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -18,6 +19,7 @@ import org.springframework.context.annotation.PropertySource;
 @EnableDiscoveryClient
 @MapperScan("com.jsy.community.mapper")
 @PropertySource(value = "classpath:common-service.properties")
+@EnableDistributedTransaction
 public class PaySystemServiceApp {
     public static void main(String[] args) {
         SpringApplication.run(PaySystemServiceApp.class);
--- proprietor/proprietor-service/src/main/java/com/jsy/community/ProprietorServiceApp.java
+++ proprietor/proprietor-service/src/main/java/com/jsy/community/ProprietorServiceApp.java
@@ -4,6 +4,7 @@ package com.jsy.community;
 import com.baomidou.mybatisplus.annotation.DbType;
 import com.baomidou.mybatisplus.extension.plugins.MybatisPlusInterceptor;
 import com.baomidou.mybatisplus.extension.plugins.inner.PaginationInnerInterceptor;
+import com.codingapi.txlcn.tc.config.EnableDistributedTransaction;
 import org.apache.dubbo.config.spring.context.annotation.DubboComponentScan;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
@@ -20,6 +21,7 @@ import org.springframework.scheduling.annotation.EnableScheduling;
 @EnableDiscoveryClient
 @MapperScan("com.jsy.community.mapper")
 @PropertySource(value = "classpath:common-service.properties")
+@EnableDistributedTransaction
 public class ProprietorServiceApp {
 	public static void main(String[] args) {
 		SpringApplication.run(ProprietorServiceApp.class, args);
