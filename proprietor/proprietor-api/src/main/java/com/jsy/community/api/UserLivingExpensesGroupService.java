77b62da55c0bd29c021c3fa7219c8a2ed2021875
ymx512422598@163.com <ymx512422598@163.com>
2021-12-03 11:43:12 +0800

    新增添加分组接口

--- proprietor/proprietor-api/src/main/java/com/jsy/community/api/UserLivingExpensesGroupService.java
+++ proprietor/proprietor-api/src/main/java/com/jsy/community/api/UserLivingExpensesGroupService.java
@@ -10,4 +10,21 @@ import com.jsy.community.entity.UserLivingExpensesGroupEntity;
  * @Version: 1.0
  **/
 public interface UserLivingExpensesGroupService extends IService<UserLivingExpensesGroupEntity> {
+    /**
+     * @author: Pipi
+     * @description: 添加生活缴费组
+     * @param groupEntity:
+     * @return: {@link Integer}
+     * @date: 2021/12/3 11:16
+     **/
+    String addGroup(UserLivingExpensesGroupEntity groupEntity);
+
+    /**
+     * @author: Pipi
+     * @description: 修改分组
+     * @param groupEntity:
+     * @return: {@link Integer}
+     * @date: 2021/12/3 11:42
+     **/
+    Integer updateGroup(UserLivingExpensesGroupEntity groupEntity);
 }
--- proprietor/proprietor-service/src/main/java/com/jsy/community/service/impl/UserLivingExpensesGroupServiceImpl.java
+++ proprietor/proprietor-service/src/main/java/com/jsy/community/service/impl/UserLivingExpensesGroupServiceImpl.java
@@ -5,7 +5,9 @@ import com.jsy.community.api.UserLivingExpensesGroupService;
 import com.jsy.community.constant.Const;
 import com.jsy.community.entity.UserLivingExpensesGroupEntity;
 import com.jsy.community.mapper.UserLivingExpensesGroupMapper;
+import com.jsy.community.utils.SnowFlake;
 import org.apache.dubbo.config.annotation.DubboService;
+import org.springframework.beans.factory.annotation.Autowired;
 
 /**
  * @Author: Pipi
@@ -15,4 +17,36 @@ import org.apache.dubbo.config.annotation.DubboService;
  **/
 @DubboService(version = Const.version, group = Const.group_proprietor)
 public class UserLivingExpensesGroupServiceImpl extends ServiceImpl<UserLivingExpensesGroupMapper, UserLivingExpensesGroupEntity> implements UserLivingExpensesGroupService {
+    @Autowired
+    private UserLivingExpensesGroupMapper groupMapper;
+
+    /**
+     * @param groupEntity :
+     * @author: Pipi
+     * @description: 添加生活缴费组
+     * @return: {@link Integer}
+     * @date: 2021/12/3 11:16
+     **/
+    @Override
+    public String addGroup(UserLivingExpensesGroupEntity groupEntity) {
+        groupEntity.setId(SnowFlake.nextId());
+        int insert = groupMapper.insert(groupEntity);
+        if (insert == 1) {
+            return String.valueOf(groupEntity.getId());
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * @param groupEntity :
+     * @author: Pipi
+     * @description: 修改分组
+     * @return: {@link Integer}
+     * @date: 2021/12/3 11:42
+     **/
+    @Override
+    public Integer updateGroup(UserLivingExpensesGroupEntity groupEntity) {
+        return null;
+    }
 }
