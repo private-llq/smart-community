1fc1b07a7424b5a1eaac48b701b88cdbf118d3e7
hu-yunfeng <1056134646@qq.com>
2021-12-08 16:50:09 +0800

    三方登录优化

--- proprietor/proprietor-web/src/main/java/com/jsy/community/controller/WeChatLoginController.java
+++ proprietor/proprietor-web/src/main/java/com/jsy/community/controller/WeChatLoginController.java
@@ -1,15 +1,13 @@
 package com.jsy.community.controller;
 
-import com.alibaba.fastjson.JSONObject;
-import com.jsy.community.annotation.ApiJSYController;
 import com.jsy.community.api.IWeChatLoginService;
 import com.jsy.community.constant.Const;
 import com.jsy.community.qo.proprietor.BindingMobileQO;
 import com.jsy.community.utils.ValidatorUtils;
-import com.jsy.community.utils.WeCharUtil;
 import com.jsy.community.vo.CommonResult;
 import com.jsy.community.vo.UserAuthVo;
 import com.zhsj.baseweb.annotation.LoginIgnore;
+import com.zhsj.baseweb.support.ContextHolder;
 import io.swagger.annotations.Api;
 import io.swagger.annotations.ApiOperation;
 import org.apache.dubbo.config.annotation.DubboReference;
@@ -29,25 +27,25 @@ public class WeChatLoginController {
 
     @DubboReference(version = Const.version, group = Const.group_proprietor, check = false)
     private IWeChatLoginService weChatLoginService;
+
     
     @LoginIgnore
     @ApiOperation("登录")
     @PostMapping("/login")
     public CommonResult login(@RequestParam("code")String code) {
-        JSONObject object = WeCharUtil.getAccessToken(code);
-        if ("".equals(object)||object==null){
-            return CommonResult.error("系统异常，请稍后再试！");
-        }
-        String accessToken = object.getString("access_token");
-        String openid = object.getString("openid");
-        return CommonResult.ok(weChatLoginService.login(openid));
+//        JSONObject object = WeCharUtil.getAccessToken(code);
+//        if ("".equals(object)||object==null){
+//            return CommonResult.error("系统异常，请稍后再试！");
+//        }
+//        String accessToken = object.getString("access_token");
+//        String openid = object.getString("openid");
+        return CommonResult.ok(weChatLoginService.loginV2(code));
     }
 
-    @LoginIgnore
     @PostMapping("/bindingMobile")
     public CommonResult bindingMobile(@RequestBody BindingMobileQO bindingMobileQO){
         ValidatorUtils.validateEntity(bindingMobileQO, BindingMobileQO.BindingMobileValidated.class);
-        UserAuthVo userAuthVo=weChatLoginService.bindingMobile(bindingMobileQO);
+        UserAuthVo userAuthVo=weChatLoginService.bindingMobileV2(bindingMobileQO, ContextHolder.getContext().getLoginUser());
         return CommonResult.ok(userAuthVo);
     }
 
