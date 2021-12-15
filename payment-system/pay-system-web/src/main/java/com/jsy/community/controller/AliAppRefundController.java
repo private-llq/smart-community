672ba1be0cc8c581f3251782eac2d710a9e96767
hu-yunfeng <1056134646@qq.com>
2021-12-15 16:43:45 +0800

    代码优化

--- payment-system/pay-system-web/src/main/java/com/jsy/community/controller/AliAppRefundController.java
+++ payment-system/pay-system-web/src/main/java/com/jsy/community/controller/AliAppRefundController.java
@@ -8,6 +8,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.DubboReference;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 // @ApiJSYController
 @Slf4j
+@RequestMapping("alipay")
 public class AliAppRefundController {
 
     @DubboReference(version = Const.version, group = Const.group_payment,check = false)
