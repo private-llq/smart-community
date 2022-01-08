dd53a95d2315c0a43f4d2be642bb1f0912050756
wodemao <1973982151@qq.com>
2022-01-08 14:25:05 +0800

    修改设置临时车入场数量的接口

--- property/property-service/src/main/java/com/jsy/community/service/impl/CarBasicsServiceImpl.java
+++ property/property-service/src/main/java/com/jsy/community/service/impl/CarBasicsServiceImpl.java
@@ -3,11 +3,14 @@ package com.jsy.community.service.impl;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.jsy.community.api.ICarBasicsService;
+import com.jsy.community.api.ICarPositionService;
 import com.jsy.community.constant.Const;
 import com.jsy.community.entity.property.CarBasicsEntity;
+import com.jsy.community.entity.property.CarPositionEntity;
 import com.jsy.community.exception.JSYError;
 import com.jsy.community.exception.JSYException;
 import com.jsy.community.mapper.CarBasicsMapper;
+import com.jsy.community.mapper.CarPositionMapper;
 import com.jsy.community.qo.property.CarBasicsMonthQO;
 import com.jsy.community.qo.property.CarBasicsRuleQO;
 import com.jsy.community.utils.SnowFlake;
@@ -17,21 +20,34 @@ import org.springframework.beans.BeanUtils;
 
 
 import javax.annotation.Resource;
+import java.util.List;
 
 @Slf4j
 @DubboService(version = Const.version, group = Const.group_property)
 public class CarBasicsServiceImpl extends ServiceImpl<CarBasicsMapper,CarBasicsEntity> implements ICarBasicsService {
     @Resource
     private CarBasicsMapper carBasicsMapper;
+    @Resource
+    private ICarPositionService positionService;
+    @Resource
+    private CarPositionMapper carPositionMapper;
 
     @Override
     public boolean addBasics(CarBasicsRuleQO carBasicsRuleQO, String uid, Long communityId) {
         CarBasicsEntity carBasicsEntity = carBasicsMapper.selectOne(new QueryWrapper<CarBasicsEntity>().eq("community_id", communityId));
+        //已使用车位（月租+业主）
+        Integer integer = positionService.selectCarPositionUseAmount(communityId);
+        // 查询小区下所有车位
+        Integer allCarPosition = carPositionMapper.getAllCarPositionByCommunity(communityId).size();
+
         if(carBasicsRuleQO.getDwellTime()<0){
             throw new JSYException(JSYError.REQUEST_PARAM.getCode(), "停留时间输入数据有误");
         }
         if (carBasicsRuleQO.getMaxNumber()<0){
             throw new JSYException(JSYError.REQUEST_PARAM.getCode(), "最大入场数输入数据有误");
+        }
+        if (carBasicsRuleQO.getMaxNumber()>(allCarPosition-integer)){
+            throw new JSYException(JSYError.REQUEST_PARAM.getCode(), "临时车入场数必须小于剩余车位数");
         }
         if(carBasicsEntity!=null){
            BeanUtils.copyProperties(carBasicsRuleQO,carBasicsEntity);
